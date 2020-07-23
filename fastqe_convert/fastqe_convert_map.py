36e962b7717708f85274027dd05b5d71c0cb7965
cjwomack <39180413+cjwomack@users.noreply.github.com>
2020-07-23 16:04:25 +1000

    The protein mapping compatible with the fastqe_convert rename

---
+++ fastqe_convert/fastqe_convert_map.py
@@ -0,0 +1,32 @@
+# only suitable for english speakers
+prot_seq_emoji_map = {
+    'A': ':green_apple:',
+    'B': ':banana:',
+    'C': ':cherries:',
+    'D': ':doughnut:',
+    'E': ':elephant:',
+    'F': ':fries:',
+    'G': ':grapes:',
+    'H': ':hamburger:',
+    'I': ':icecream:',
+    'J': ':jeans:',
+    'K': ':key:',
+    'L': ':lemon:',
+    'M': ':mushroom:',
+    'N': ':nose:',
+    'O': ':octopus:',
+    'P': ':pineapple:',
+    'Q': ':princess:',
+    'R': ':rabbit:',
+    'S': ':strawberry:',
+    'T': ':tomato:',
+    'U': ':umbrella:',
+    'V': ':volcano:',
+    'W': ':watermelon:',
+    'X': ':x:',
+    'Y': ':sailboat:',
+    '*': ':hand:',
+    '-': ':wavy_dash:',
+}
+
+prot_scale = "ABCDEFGHIJKLMNOPQRSTUVWXY*-"
