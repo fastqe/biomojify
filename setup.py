82a544926915b01984322c0d2da3624b3334d0bb
Andrew Lonsdale <Andrew.Lonsdale@petermac.org>
2020-07-26 19:14:57 +1000

    Fix setip

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-LONG_DESCRIPTION = """The program reads one or more input FASTA files.
-For each file it computes a variety of statistics, and then
-prints a summary of the statistics as output.
-
-The goal is to provide a solid foundation for new bioinformatics command line tools,
-and is an ideal starting place for new projects."""
+LONG_DESCRIPTION = """The program reads one or more input FASTA or FASTQ files and converts them to emoji."""
 
 
 setup(
     name="biomojify",
     version="0.1.0.0",
-    author="FASTQE-CONVERT_AUTHOR",
-    author_email="FASTQE-CONVERT_EMAIL",
+    author="Andrew Lonsdale",
+    author_email="andrew.lonsdale@lonsbio.com.au",
     packages=["biomojify"],
     package_dir={"biomojify": "biomojify"},
     entry_points={
@@ -22,7 +17,7 @@ setup(
     },
     url="https://github.com/fastqe/biomojify",
     license="LICENSE",
-    description=("A prototypical bioinformatics command line tool"),
+    description=("Convert FASTQE and FASTA files to emoji."),
     long_description=(LONG_DESCRIPTION),
     install_requires=["biopython","fastqe"],
 )
